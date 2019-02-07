556b8cb61a04457f0ffc8ed02d7ad0e6f9e812f4
visualstudio <visualstudio@230-PC07>
2019-02-07 15:11:10 -0500

    3.09pm commit

--- C0707995/Program.cs
+++ C0707995/Program.cs
@@ -9,7 +9,63 @@ namespace C0707995
     class Program
     {
         static void Main(string[] args)
+
+
         {
+            var a = new TestQuestion2();
+            a.PlayingWithForLoops();
         }
     }
+
+
+    class TestQuestion2
+
+    {
+        public int myFavVariable = 0;
+        public void PlayingWithForLoops()
+        {
+            for (; MyMthod();)
+            {
+                if (myFavVariable > 10)
+                {
+                    Console.WriteLine("i ma so out of here");
+                    break;
+                }
+                Console.WriteLine("i am in for loop");
+                
+
+            }
+        }
+        public bool MyMthod()
+        {
+            myFavVariable++;
+            return true;
+        }
+    }
+    class dog
+    {
+        public dog(string name, string breed)
+        {
+            dogName = name;
+            dogbreed = breed;
+        }
+        public string dogName;
+        public string dogbreed;
+        public dog next_dog;
+        public dog previous_dog;
+           
+    }
+    class birthdayParty
+    {   
+        public dog fifo;
+        public dog poodle;
+        public dog clarance;
+        public dog roy;
+        public dog germanSheppard;
+        public dog head;
+        public dog tail;
+        public dog temporary;
+
+    }
+
 }
